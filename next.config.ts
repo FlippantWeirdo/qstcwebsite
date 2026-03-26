d0802e197fcf8fdfd008910f46bbed7c70e1b6cf
FlippantWeirdo <abdulmuiza@outlook.com>
2026-03-26 22:22:23 +0100

    chore: redirects

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,40 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/about-us",
+        destination: "/about",
+        permanent: true,
+      },
+      {
+        source: "/contact-us",
+        destination: "/contact",
+        permanent: true,
+      },
+      {
+        source: "/our-solutions",
+        destination: "/projects",
+        permanent: true,
+      },
+      {
+        source: "/electrical-power-systems",
+        destination: "/electric-power-solutions",
+        permanent: true,
+      },
+      {
+        source: "/low-voltage-security-systems",
+        destination: "/elvsystems",
+        permanent: true,
+      },
+      {
+        source: "/it-infrastructure-solutions",
+        destination: "/it-infrastructure",
+        permanent: true,
+      },
+    ];
+  },
   /* config options here */
 };
 
